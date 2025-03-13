2a8a837d45daa34845c0f5cca5c1a13d30384f52
matthias-own3d <matthias-own3d@users.noreply.github.com>
2025-03-13 15:00:55 +0000

    Auto deploy from Github Actions

---
+++ assets/index.html-wGmvXdO9.js
@@ -0,0 +1,1 @@
+import{_ as n,r as o,o as t,c,d as r}from"./app-ALAXI9dN.js";const _={};function s(l,a){const e=o("build");return t(),c("div",null,[r(e)])}const i=n(_,[["render",s],["__file","index.html.vue"]]);export{i as default};
